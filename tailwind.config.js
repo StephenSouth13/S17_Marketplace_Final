242bcee2d3909140b9710240bddd98a6c1fe773b
Quách Thành Long <stephensouth1307@gmail.com>
2025-10-11 09:17:16 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        shop_light_green: '#3b9c3c',
+        shop_dark_green: '#2a7c2b',
+        shop_gold: '#d4af37',
+        shop_gray: '#f5f5f5',
+        shop_black: '#111111',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require("autoprefixer"),
+  ],
 }
