bde3e4e78ea0e105db4de9fd2a5f6f10a63a4463
Quách Thành Long <stephensouth1307@gmail.com>
2025-10-13 10:33:47 +0700

    Update utils.ts

--- sanity/utils.ts
+++ sanity/utils.ts
@@ -1,7 +1,15 @@
-import { client } from "./lib/client";
-import { SERVICES_QUERY } from "./queries/query";
+import { client } from "@/sanity/lib/client";
 
 export async function getServices(limit?: number) {
-  const result = await client.fetch(SERVICES_QUERY);
-  return limit ? result.slice(0, limit) : result;
+  const query = `
+    *[_type == "service"] | order(_createdAt desc)${limit ? `[0...${limit}]` : ""} {
+      _id,
+      title,
+      "slug": slug.current,
+      excerpt,
+      "mainImage": { "asset": { "url": mainImage.asset->url } },
+      category->{ title }
+    }
+  `;
+  return await client.fetch(query);
 }
